be073bb261a2d48b300bc499f8c604ed0018c77b
ArchitectAK <ankitdroiddeveloper@gmail.com>
2023-02-15 18:51:02 +0800

    Modify hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_API_KEY = "";
+const ROPSTEN_PRIVATE_KEY = "";
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    "ropsten": {
+      url:`https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts:[`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
